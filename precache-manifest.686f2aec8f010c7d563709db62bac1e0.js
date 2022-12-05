2294e5082e272f4b34493ec621c745bdfda8f913
carbondal <allan.porter@state.co.us>
2022-12-05 10:42:05 -0700

    Updates

---
+++ precache-manifest.686f2aec8f010c7d563709db62bac1e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a7304245b147f00c909",
+    "url": "/CameraCode/static/css/main.b65ff8ab.chunk.css"
+  },
+  {
+    "revision": "2a7304245b147f00c909",
+    "url": "/CameraCode/static/js/main.2a730424.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/CameraCode/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/CameraCode/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "881161ff30189ea79a8d",
+    "url": "/CameraCode/static/js/runtime~main.881161ff.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/CameraCode/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9ee749001a793bae1f2e9cfd2ce5488d",
+    "url": "/CameraCode/index.html"
+  }
+];
